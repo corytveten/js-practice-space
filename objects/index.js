7ded7d7255f1b019611376bac0f8f1d6980aea58
Cory Tveten <corytveten@gmail.com>
2021-08-25 22:18:16 -0400

    do while

--- objects/index.js
+++ objects/index.js
@@ -14,3 +14,21 @@ console.log(myDog)
 
 //js has a hasOwnProperty method that can be called on an object with the property name as the argument
 
+//below is a method to create a copy of an object
+//var objectCopy = JSON.parse(JSON.stringify(originalObject))
+
+//multidimensional array is another name for nested arrays
+
+//do..while loops
+//will always run at least one time and then check the condition
+//do is a keyword
+
+const myArray = [];
+var i = 10;
+
+do {
+    myArray.push(i);
+    i++;
+} while (i < 5)
+
+console.log(i, myArray);
