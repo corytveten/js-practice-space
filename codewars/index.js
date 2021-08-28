c7741a133ce15e12d3e748467e0f9355a319aa09
Cory Tveten <corytveten@gmail.com>
2021-08-28 14:22:10 -0400

    codewars practicce

---
+++ codewars/index.js
@@ -0,0 +1,32 @@
+// function reverseString(str) {
+//   // const revArr = str.split("").reverse();
+//   // return revArr.join(''); 
+//   return str.split("").reverse().join("");
+// }
+
+// console.log(reverseString('world'))
+
+// function songDecoder(wubStr) {
+//   const lyricArr = wubStr.split("WUB").filter(elem => {
+//     return elem.length > 0
+//   })
+//   return lyricArr.join(" ")
+// }
+
+// console.log(songDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"))
+
+// function dnaStrand(str) {
+//     const chars = {'A':'T', 'T':'A', 'G':'C', 'C':'G'}
+//     // const splitStr = str.split('');
+//     return str.replace(/[ATGC]/g, m => chars[m])
+    
+//   }
+  
+//   dnaStrand('ATTGC')
+
+//remove first and last characters of a string
+function removeFirstLast(str) {
+    const removed = str.substring(1, str.length - 1)
+    console.log(removed)
+    //return firstOff.replace(firstOff.charAt(firstOff.length -1), '')
+}
