6825f3fee5262a3631c44b9c458165a73daa7283
Cory Tveten <corytveten@gmail.com>
2021-09-21 21:44:10 -0400

    more codewars

--- codewars/index.js
+++ codewars/index.js
@@ -52,100 +52,129 @@
 //     return vowelsCount
 // }
 
-function arithmetic(a, b, operator) {
-    const symbolConverter = {
-        'add': function (x, y) { return x + y },
-        'subtract': function (x, y) { return x - y },
-        'multiply': function (x, y) { return x * y },
-        'divide': function (x, y) { return x / y }
-    }
-    return symbolConverter[operator](a, b)
-    // if (operator === 'add') {
-    //     return a + b;
-    // } else if (operator === 'subtract') {
-    //     return a - b;
-    // } else if (operator === 'multiply') {
-    //     return a * b;
-    // } else if (operator === 'divide') {
-    //     return a / b;
-    // } else {
-    //     return 'enter an operator'
-    // }
-}
+// function arithmetic(a, b, operator) {
+//     const symbolConverter = {
+//         'add': function (x, y) { return x + y },
+//         'subtract': function (x, y) { return x - y },
+//         'multiply': function (x, y) { return x * y },
+//         'divide': function (x, y) { return x / y }
+//     }
+//     return symbolConverter[operator](a, b)
+//     // if (operator === 'add') {
+//     //     return a + b;
+//     // } else if (operator === 'subtract') {
+//     //     return a - b;
+//     // } else if (operator === 'multiply') {
+//     //     return a * b;
+//     // } else if (operator === 'divide') {
+//     //     return a / b;
+//     // } else {
+//     //     return 'enter an operator'
+//     // }
+// }
 
-//the wave
-function wave(str) {
-    const waveArr = []
-    for (let i=0; i<str.length; i++) {
-        if (str.charAt(i) === " ") {
-            null
+// //the wave
+// function wave(str) {
+//     const waveArr = []
+//     for (let i=0; i<str.length; i++) {
+//         if (str.charAt(i) === " ") {
+//             null
+//         } else {
+//             waveArr.push(str.slice(0, i) + str.charAt(i).toUpperCase() + str.slice(i+1))
+//         }
+//     };
+//     return waveArr.join(" ")
+// }
+
+// //increments a string by 1 to create a new string 
+// function incrementString (strng) {
+//     if (isNaN(strng.slice(-1))) {
+//         return strng + 1 
+//     } else {
+//         strArr = strng.split('');
+//         const firstNumIndex = strArr.indexOf(strArr.find(x => !isNaN(x)));
+//         const lastNum = parseInt(strng.slice(firstNumIndex - strng.length)) + 1;
+//         return strng.slice(0, firstNumIndex) + lastNum
+//     }
+//   }
+
+// function timer() {
+//     for (let i=0; i<20; i++) {
+//         (function(i) {
+//             setTimeout(function() {
+//                 console.log(i);
+//         }, 1000);
+//     })(i);
+// }
+// }
+
+// timer()
+
+// function divisors(num) {
+//     const divArr = []
+//    for (let i=1; i<=num; i++) {
+//         if (num % i === 0) {
+//             divArr.push(i)
+//         } 
+//         console.log(divArr)
+//         return divArr.length
+//    } 
+// }
+
+// function factorial(n) {
+//     let answer = 1;
+//     if (n == 0 || n == 1){
+//       return answer;
+//     }else{
+//       for(var i = n; i >= 1; i--){
+//         answer = answer * i;
+//       }
+//       return answer;
+//     }  
+//   }
+
+//   function twoSum(numbers, target) {
+
+//   }
+
+//   const factors = number => Array
+//     .from(Array(number + 1), (_, i) => i)
+//     .filter(i => number % i === 0)
+
+// console.log(factors(36));
+
+// function domainName(url){
+//     const dom = url.split('//')[1] || url
+//     const dom2 = dom.split('/')[0]
+//     const dom3 = dom2.replace('www.', '')
+//     return dom3.split('.')[0]
+//   }
+
+  var maxSequence = function(arr){
+    let currentSum = 0;
+    let largestSum = 0;
+
+    for (let number of arr) {
+        currentSum = Math.max(0, (currentSum + number))
+        largestSum = Math.max(largestSum, currentSum)
+    }
+
+    return largestSum
+  }
+
+  function dirReduc(arr){
+    const opposites = {'NORTH': 'SOUTH', 'EAST': 'WEST', 'SOUTH': 'NORTH', 'WEST': 'EAST'};
+    const result = []
+    arr.forEach(item => {
+        if(result.length) {
+            const prevItem = result.pop()
+            if(prevItem !== opposites[item]) {
+                result.push(prevItem)
+                result.push(item)
+            }
         } else {
-            waveArr.push(str.slice(0, i) + str.charAt(i).toUpperCase() + str.slice(i+1))
+            result.push(item)
         }
-    };
-    return waveArr.join(" ")
+    })
+    return result
 }
-
-//increments a string by 1 to create a new string 
-function incrementString (strng) {
-    if (isNaN(strng.slice(-1))) {
-        return strng + 1 
-    } else {
-        strArr = strng.split('');
-        const firstNumIndex = strArr.indexOf(strArr.find(x => !isNaN(x)));
-        const lastNum = parseInt(strng.slice(firstNumIndex - strng.length)) + 1;
-        return strng.slice(0, firstNumIndex) + lastNum
-    }
-  }
-
-function timer() {
-    for (let i=0; i<20; i++) {
-        (function(i) {
-            setTimeout(function() {
-                console.log(i);
-        }, 1000);
-    })(i);
-}
-}
-
-timer()
-
-function divisors(num) {
-    const divArr = []
-   for (let i=1; i<=num; i++) {
-        if (num % i === 0) {
-            divArr.push(i)
-        } 
-        console.log(divArr)
-        return divArr.length
-   } 
-}
-
-function factorial(n) {
-    let answer = 1;
-    if (n == 0 || n == 1){
-      return answer;
-    }else{
-      for(var i = n; i >= 1; i--){
-        answer = answer * i;
-      }
-      return answer;
-    }  
-  }
-
-  function twoSum(numbers, target) {
-
-  }
-
-  const factors = number => Array
-    .from(Array(number + 1), (_, i) => i)
-    .filter(i => number % i === 0)
-
-console.log(factors(36));
-
-function domainName(url){
-    const dom = url.split('//')[1] || url
-    const dom2 = dom.split('/')[0]
-    const dom3 = dom2.replace('www.', '')
-    return dom3.split('.')[0]
-  }
