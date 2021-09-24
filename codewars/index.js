5fefdb424dfbb46773aafa80e19a4009d75fde5b
Cory Tveten <corytveten@gmail.com>
2021-09-23 20:52:38 -0400

    add codewars

--- codewars/index.js
+++ codewars/index.js
@@ -178,3 +178,12 @@
     })
     return result
 }
+
+function bouncingBall(h,  bounce,  window) {
+    const cond = h > 0 && (bounce > 0 && bounce < 1) && window < h;
+    if (cond) {
+        return h < window ? -1 : 2 + bouncingBall((h * bounce), bounce, window);
+    } else {
+        return -1;
+    }
+}
